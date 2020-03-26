a9f8ba9d4766169b545e3d3bba4ef053de267184
Gustavo Garcia-Salas <gggustavogs@gmail.com>
2020-03-25 19:56:32 -0600

    Using packages in java

---
+++ 29Packages/src/main/java/java/Packages.java
@@ -0,0 +1,22 @@
+package java;
+
+
+import static com.java.Utility.print;
+
+
+public class Packages {
+    
+    public static void main(String[] args) {
+        //Implementing the class' name correctly
+        //fully qualified name
+        //com.gm.Utileria.imprimir("Hola");
+        
+        //haciendo un import de la clase
+        //Class import
+        //Utility.print("hi");
+        
+        //import static
+        print("hi");
+    }
+    
+}
